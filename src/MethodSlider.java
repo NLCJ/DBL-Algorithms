e8a2f4e0e5950bc5589badc82444237806edc6a1
KozlovIvan <ivan.klaipeda@gmail.com>
2015-04-23 16:50:28 +0200

    added slider

--- src/MethodSlider.java
+++ src/MethodSlider.java
@@ -11,5 +11,14 @@
  * @author Ivan Kozlov
  */
 public class MethodSlider {
-    
+    public void OutputSLider(String s, int w, int h, int n_p, int[][] p){
+        System.out.println("placement model: "+ s);
+            System.out.println("width: "+w);
+            System.out.println("height: "+h);
+            System.out.println("number of points: "+n_p);
+            System.out.println("nubmers of labels: "+n_p);
+            for(int i=0;i<n_p;i++){
+                System.out.println(p[i][0]+" "+p[i][1]+"0.5");
+            }
+    }
 }
