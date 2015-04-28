1e40274bd1ad2b8fd676b98bc90ea143b9d4c4d8
KozlovIvan <ivan.klaipeda@gmail.com>
2015-04-29 01:56:59 +0200

    started shift calculator for slider

--- src/MethodSlider.java
+++ src/MethodSlider.java
@@ -3,6 +3,19 @@
  * @author Ivan Kozlov
  */
 public class MethodSlider {
+    
+    public int[][] ShiftCalculator(int w, int h, int n_p,int[][] p){
+        int[][] pas = new int[p.length][3];
+        for(int i=0;i<p.length;i++){
+          pas[i][0]=p[i][0];
+          pas[i][1]=p[i][1];
+          pas[i][2]=0;
+        }
+        
+    return pas;}
+    
+    
+    
     public void OutputSlider(String s, int w, int h, int n_p, int[][] p){
         System.out.println("placement model: "+ s);
             System.out.println("width: "+w);
