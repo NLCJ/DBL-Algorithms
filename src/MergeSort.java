e95bce43ccf9d265560eca903fae9a47bcd7f4a2
KozlovIvan <ivan.klaipeda@gmail.com>
2015-04-29 10:58:54 +0200

    ONE MAIN TO RULE THEM ALL

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,16 +1,9 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author s130973
@@ -98,7 +91,5 @@ public class MergeSort {
 
     }
 
-    public static void main(String[] args) {
-        new MergeSort().begin();
-    }
+    
 }
