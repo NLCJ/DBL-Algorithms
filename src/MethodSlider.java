b626121bc6454321859fcfb484bec04c403b301a
KozlovIvan <ivan.klaipeda@gmail.com>
2015-04-29 09:12:09 +0200

    small change in slider file

--- src/MethodSlider.java
+++ src/MethodSlider.java
@@ -4,26 +4,28 @@
  */
 public class MethodSlider {
     
-    public int[][] ShiftCalculator(int w, int h, int n_p,int[][] p){
-        int[][] pas = new int[p.length][3];
-        for(int i=0;i<p.length;i++){
-          pas[i][0]=p[i][0];
-          pas[i][1]=p[i][1];
-          pas[i][2]=0;
-        }
+    public int[][] ShiftCalculator(int w, int h, int[][] p){
+            int[][] pas = new int[p.length][3];
+            for(int i=0;i<p.length;i++){
+                pas[i][0]=p[i][0];
+                pas[i][1]=p[i][1];
+                pas[i][2]=0;
+            }
         
-    return pas;}
+            return pas;
+    }
     
     
     
     public void OutputSlider(String s, int w, int h, int n_p, int[][] p){
-        System.out.println("placement model: "+ s);
+            int [][] output = ShiftCalculator(w,h,p);
+            System.out.println("placement model: "+ s);
             System.out.println("width: "+w);
             System.out.println("height: "+h);
             System.out.println("number of points: "+n_p);
             System.out.println("nubmers of labels: "+n_p);
             for(int i=0;i<n_p;i++){
-                System.out.println(p[i][0]+" "+p[i][1]+" 0.5");
+                System.out.println(output[i][0]+" "+output[i][1]+" "+output[i][2]);
             }
     }
 }
