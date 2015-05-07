6b99b5ab1e16ab0aa3466f32e63c240a90dfab4e
KozlovIvan <ivan.klaipeda@gmail.com>
2015-05-07 15:36:52 +0200

    ok, sorta of working drawing shit, fixed the panel
    now points left to plot

--- src/PlotPanel.java
+++ src/PlotPanel.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import javax.swing.*;
 /**
  *
@@ -20,7 +21,13 @@ public class PlotPanel extends JPanel {
     @Override
     public void paint (Graphics g){
         Graphics2D g2d = (Graphics2D) g;
-        super.paint(g);
+           super.paint(g2d);
+           this.setBackground(Color.red);
+           
+           g2d.setColor(Color.WHITE);
+           g2d.fillOval(69, 69, 50, 50);
+           g2d.drawString("KURWA", 500, 500);
+      
     }
     
     
