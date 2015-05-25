2bfb60a3612b697fabe5026249cbddd828d8a40c
Erik-Boom37 <e.f.j.boom@student.tue.nl>
2015-05-25 13:27:53 +0200

    Added Contracts 4-pos
    See title. You should be able to work in it now, as this is the
    structure I had in mind.

--- src/Method4Pos.java
+++ src/Method4Pos.java
@@ -1,11 +1,12 @@
-
+import java.util.Set;
 /**
  *
  * @author Ivan Kozlov
  */
 public class Method4Pos {
-
+    private double c;// the temperature in the annealing schedule
     private Point[] result;
+    private Set<Label> collisions;
     MergeSort mergesort = new MergeSort();
     QuadTree quad = new QuadTree(10, 0, 20, 0, 20);
 
@@ -28,12 +29,66 @@ public class Method4Pos {
 
         return originalOrder;
     }
-     public void quadtree(Point[] points) {
+    public void quadtree(Point[] points) {
         for (Point p : points) {
                 quad.insert(p);
            
         }
     }
+    
+    /**
+     * Gives all points a random initial label placement
+     * @param p the list points to give the random labels
+     */
+     
+    public void RandomInitialPosition(Point[] p){
+    }
+    /**
+     * Finds the collisions of the labels
+     * @param p The points that has the labels
+     * @return the actual collisions
+     */
+    public Set<Label> FindCollisions(Point[] p){
+        return null;
+    }
+    /**
+     * Changes a label of a random point to a random position
+     * @param p The array containing a point you wish to change
+     */
+    public void ChangeRandomLabel(Point[] p){
+    }
+    /**
+     * The actual annealing schedule
+     */
+    public void Annealing(){
+    }
+    /**
+     * Calculates the acceptance chance
+     * @param Oldscore The "old" number of collisions
+     * @param NewScore The "new" number of collisions     
+     * @return the acceptance chance
+     */
+    
+   
+    public double AcceptanceChance( double Oldscore, double NewScore){
+        return 1.0;
+    }
+    
+    /**
+     * Reverts the change back to the position before the change of the random 
+     * label.
+     * @param p the same as ever
+     */
+    public void RevertChanges(Point[] p){
+    }
+    /**
+     * Removes the collisions of the solution, if there are any
+     * @param p The list of points
+     */
+    public void RemoveCollisions(Point[] p){
+    }
+            
+            
     public void Output4Position(String s, int w, int h, int n_p, Point[] p) {
         //Reorder the points to the original order
         Point[] output = PositionCalculator(w, h, p);
