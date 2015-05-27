4194b4cab58cdadb2502ae19a28ab6c38255e491
Janice <angelnais@hotmail.com>
2015-05-27 15:39:18 +0200

    input.txt changed

--- src/MainReader.java
+++ src/MainReader.java
@@ -90,6 +90,7 @@ class MainReader {
         try {
 
             File file = new File("data-of-awesomeness.txt");
+            file = new File("input.txt");
             Scanner sc = new Scanner(file);
 
             // Get the model data
