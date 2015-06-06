0faf6ea8e77440072912d1392e9cc7875239f1df
diederikw <diederikw@hotmail.com>
2015-06-06 15:20:02 +0200

    data generator used for cluster data

--- src/DataGeneratorForDiederik.java
+++ src/DataGeneratorForDiederik.java
@@ -23,19 +23,29 @@ public class DataGeneratorForDiederik {
     String placementModel;
     int labelWidth;
     int labelHeight;
-    int amountOfPoints;
-    int minAxisX/* = 0*/;
-    int minAxisY/* = 0*/;
-    int maxAxisX/* = 10000*/;
-    int maxAxisY/* = 10000*/;
-    int minX;
-    int maxX;
-    int minY;
-    int maxY;
+    int amountOfPointsLeft;
+    int minAxisXLeft/* = 0*/;
+    int minAxisYLeft/* = 0*/;
+    int maxAxisXLeft/* = 10000*/;
+    int maxAxisYLeft/* = 10000*/;
+    int minXLeft;
+    int maxXLeft;
+    int minYLeft;
+    int maxYLeft;
+    int amountOfPointsRight;
+    int minAxisXRight/* = 0*/;
+    int minAxisYRight/* = 0*/;
+    int maxAxisXRight/* = 10000*/;
+    int maxAxisYRight/* = 10000*/;
+    int minXRight;
+    int maxXRight;
+    int minYRight;
+    int maxYRight;
     double accuracy;
     PrintWriter file;
     Scanner input;
     String distribution;
+    int amountOfPoints;
     
     // Constructor
     private void askData() {
@@ -44,23 +54,38 @@ public class DataGeneratorForDiederik {
             input = new Scanner( System.in ).useLocale( Locale.US );
             for (int i = 0; i < 100; i++){
             // Store the variables
-            this.placementModel = "2pos";
+            this.placementModel = "1slider";
             this.distribution = "Cluster";
-            this.accuracy = 100;
-            this.labelWidth = 10;
-            this.labelHeight = 10;
-            this.amountOfPoints = 10;
-            this.minAxisX = 0;
-            this.minAxisY = 0;
-            this.maxAxisX = 10000;
-            this.maxAxisY = 10000;
-            this.minX = 0;
-            this.maxX = 10000;
-            this.minY = 0;
-            this.maxY = 10000;
-            
+            this.accuracy = 50;
+            this.labelWidth = 200;
+            this.labelHeight = 200;
+            //Half of the total points you want
+            this.amountOfPointsLeft = 5000;
+            //Definition of the Left half
+            this.minAxisXLeft = 0;
+            this.minAxisYLeft = 0;
+            this.maxAxisXLeft = 5000;
+            this.maxAxisYLeft = 10000;
+            //The position of the left cluster
+            this.minXLeft = 0;
+            this.maxXLeft = 2000;
+            this.minYLeft = 8000;
+            this.maxYLeft = 10000;
+            //Half of the total points you want
+            this.amountOfPointsRight = 5000;
+            //Definition of the Right half
+            this.minAxisXRight = 5000;
+            this.minAxisYRight = 0;
+            this.maxAxisXRight = 5000;
+            this.maxAxisYRight = 10000;
+            //The position of the right cluster
+            this.minXRight = 8000;
+            this.maxXRight = 10000;
+            this.minYRight = 0;
+            this.maxYRight = 2000;
+            this.amountOfPoints = amountOfPointsLeft+amountOfPointsRight;
             // Initialize the file
-            file = new PrintWriter("Experimental Data\\Cluster\\"+placementModel+distribution+labelWidth+"x"+amountOfPoints+"#"+(i+1)+".txt", "UTF-8");
+            file = new PrintWriter("Experimental Data\\"+placementModel+distribution+labelWidth+"x"+amountOfPoints+"#"+(i+1)+".txt", "UTF-8");
             
             // Print the default information
             file.println( "placement model: " + this.placementModel );
@@ -78,21 +103,45 @@ public class DataGeneratorForDiederik {
     
     // Generate the output
     private void output() {
-        // For each point
-        for( int i = 0; i < amountOfPoints; i++ ) {
+        // For each point on the left side of the data
+        for( int i = 0; i < amountOfPointsLeft; i++ ) {
             // Generate within limits given
-            int accurateMinX = this.minX;
-            int accurateMinY = this.minY;
-            int accurateMaxX = this.maxX;
-            int accurateMaxY = this.maxY;
+            int accurateMinX = this.minXLeft;
+            int accurateMinY = this.minYLeft;
+            int accurateMaxX = this.maxXLeft;
+            int accurateMaxY = this.maxYLeft;
                 
             // Create random to test against accuracy
             if( randInt( 0, 100 ) > accuracy ) {
                 // Random over whole field
-                accurateMinX = this.minAxisX;
-                accurateMinY = this.minAxisY;
-                accurateMaxX = this.maxAxisX;
-                accurateMaxY = this.maxAxisY;
+                accurateMinX = this.minAxisXLeft;
+                accurateMinY = this.minAxisYLeft;
+                accurateMaxX = this.maxAxisXLeft;
+                accurateMaxY = this.maxAxisYLeft;
+            }
+            
+            // Get the x and y value
+            int xValue = randInt( accurateMinX, accurateMaxX );
+            int yValue = randInt( accurateMinY, accurateMaxY );
+            
+            // Write this point to the file
+            file.println( xValue + " " + yValue );
+        }
+        // For each point on the right side of the data
+        for( int i = 0; i < amountOfPointsRight; i++ ) {
+            // Generate within limits given
+            int accurateMinX = this.minXRight;
+            int accurateMinY = this.minYRight;
+            int accurateMaxX = this.maxXRight;
+            int accurateMaxY = this.maxYRight;
+                
+            // Create random to test against accuracy
+            if( randInt( 0, 100 ) > accuracy ) {
+                // Random over whole field
+                accurateMinX = this.minAxisXRight;
+                accurateMinY = this.minAxisYRight;
+                accurateMaxX = this.maxAxisXRight;
+                accurateMaxY = this.maxAxisYRight;
             }
             
             // Get the x and y value
